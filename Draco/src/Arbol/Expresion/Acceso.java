48bab8fbd63174849cca2fd4e48dbc432edac8f9
Alina <alin2525@gmail.com>
2018-10-30 10:34:12 -0600

    Cambio de la estructura para manejar el acceso

--- Draco/src/Arbol/Expresion/Acceso.java
+++ Draco/src/Arbol/Expresion/Acceso.java
@@ -5,7 +5,10 @@
  */
 package Arbol.Expresion;
 
+import ASM.Generador;
+import ASM.elementoRetorno;
 import Arbol.objetoBase;
+import D_Mas_Mas.Tabla_Simbolos.TablaSimbolos;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +18,44 @@ import java.util.List;
  */
 public class Acceso extends objetoBase{
     
-    public List<objetoBase>elementos;
+     public int tipoAcceso;
+     public objetoBase valor1;
+     public objetoBase valor2;
+     public objetoBase valor3;
+  
+     
     
+     public Acceso(Object tipo, Object val1 ){
+         this.tipoAcceso=(int)tipo;
+         this.valor1= (objetoBase)val1;
+     }
+     
+      public Acceso(Object tipo, Object val1, Object val2 ){
+         this.tipoAcceso=(int)tipo;
+         this.valor1= (objetoBase)val1;
+         this.valor2= (objetoBase)val2;
+         
+     }
     
-    public Acceso(Object valores){
-        this.elementos = (ArrayList<objetoBase>)valores;
+      
+    public Acceso(Object tipo, Object val1, Object val2, Object val3 ){
+         this.tipoAcceso=(int)tipo;
+         this.valor1= (objetoBase)val1;
+         this.valor2= (objetoBase)val2;
+          this.valor3= (objetoBase)val3;
+     }
+      
+    
+   
+
+    @Override
+    public elementoRetorno ejecutar(Generador cod, TablaSimbolos tabla) {
+        return super.ejecutar(cod, tabla); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    
+    
+    
+    
+   
 }
